a233cc124811e61f8e101f6542c8d3901c73f2f9
Krystian Majewski <krystian.majewski@aixoft.com>
2020-09-26 10:33:52 +0000

    Merged in feature/TID-15-configure-building-on-pull-request (pull request #3)

---
+++ src/test/java/com/aixoft/escassandra/MainTest.java
@@ -0,0 +1,13 @@
+package com.aixoft.escassandra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    void simplyTest() {
+        assertTrue(true);
+    }
+}
