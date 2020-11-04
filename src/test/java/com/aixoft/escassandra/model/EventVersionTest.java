d49ca65b800b4e06daa7a51c6b034e0e9fe10b12
Tomasz Bawor <bawortomasz@gmail.com>
2020-11-04 19:12:45 +0100

    Add example unit test

---
+++ src/test/java/com/aixoft/escassandra/model/EventVersionTest.java
@@ -0,0 +1,21 @@
+package com.aixoft.escassandra.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EventVersionTest {
+
+    @Test
+    @DisplayName("Initial event version should be 0.0")
+    void shouldHaveInitialVersionZeroZero() {
+
+        // when
+        EventVersion ev = EventVersion.initial();
+
+        // then
+        assertEquals(0, ev.getMajor());
+        assertEquals(0, ev.getMinor());
+    }
+}
