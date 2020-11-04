9408aba4d9648f2d0652deeb7858a67e1056d332
Tomasz Bawor <bawortomasz@gmail.com>
2020-11-04 22:28:15 +0100

    Add complete EventVersionTest

--- src/test/java/com/aixoft/escassandra/model/EventVersionTest.java
+++ src/test/java/com/aixoft/escassandra/model/EventVersionTest.java
@@ -10,12 +10,39 @@ class EventVersionTest {
     @Test
     @DisplayName("Initial event version should be 0.0")
     void shouldHaveInitialVersionZeroZero() {
-
         // when
         EventVersion ev = EventVersion.initial();
 
         // then
         assertEquals(0, ev.getMajor());
         assertEquals(0, ev.getMinor());
+    }
+
+    @Test
+    @DisplayName("Should increment major version when snapshot")
+    void shouldIncrementMinorWhenSnapshot() {
+        // given
+        EventVersion initial = EventVersion.initial();
+
+        // when
+        EventVersion eventVersion = initial.getNext(true);
+
+        // then
+        assertEquals(1, eventVersion.getMajor());
+        assertEquals(0, eventVersion.getMinor());
+    }
+
+    @Test
+    @DisplayName("Should increment minor version when not snapshot")
+    void shouldIncrementMajorWhenNotSnapshot() {
+        // given
+        EventVersion initial = EventVersion.initial();
+
+        // when
+        EventVersion eventVersion = initial.getNext(false);
+
+        // then
+        assertEquals(0, eventVersion.getMajor());
+        assertEquals(1, eventVersion.getMinor());
     }
 }
